1c97f32581240b46a9aa6782c4d8fd3a631e6b4d
Joe Ulyatt <joewhocodes@gmail.com>
2025-04-22 11:37:18 +0200

    fix: :bug: Fix error in dummy url

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,2 +1,2 @@
-export const DUMMY_API_GET = 'https://jsonplaceholder.typicode.com/posts/1'
-export const DUMMY_API_POST = 'https://jsonplaceholder.typicode.com/posts/1'
+export const DUMMY_API_GET_URL = 'https://jsonplaceholder.typicode.com/posts/1'
+export const DUMMY_API_POST_URL = 'https://jsonplaceholder.typicode.com/posts'
